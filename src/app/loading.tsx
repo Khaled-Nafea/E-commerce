1c1781c3e9f9b190db184bc3107acd0c6b5e6d83
Khaled-Nafea <khalednafea2000@gmail.com>
2026-03-07 15:43:03 +0300

    feat: initial commit

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div>
+      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <div className="loader"></div>
+      </div>
+    </div>
+  );
+}
